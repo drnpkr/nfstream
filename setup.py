ae865b09e1fea5f54ab03268f85d34a600624ce1
Adrian Pekar <adrian.pekar@gmail.com>
2025-07-09 13:07:18 +0200

    Bump version to 6.6.0 for nDPI 4.14 upgrade

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ else:
 
 setup(
     name="nfstream",
-    version="6.5.5",
+    version="6.6.0",
     url="https://www.nfstream.org/",
     license="LGPLv3",
     description="A Flexible Network Data Analysis Framework",

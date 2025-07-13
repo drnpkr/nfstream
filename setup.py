657664de70592c14a9883a8c7abd2f8fde810edd
Adrian Pekar <adrian.pekar@gmail.com>
2025-07-13 14:21:59 +0200

    Bump version: 6.7.1 → 6.7.2

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ else:
 
 setup(
     name="nfstream",
-    version="6.7.1",
+    version="6.7.2",
     url="https://www.nfstream.org/",
     license="LGPLv3",
     description="A Flexible Network Data Analysis Framework",
